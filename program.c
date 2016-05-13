181ec443f89e5a9c5fca151532ef94ca964619f8
Samuel Wallace <wallacs5@miamioh.edu>
2016-05-13 14:11:18 -0400

    1.0 Release Version
    Very Large Commit- Finishes  program.c adding export to XML
    functionality. Also adds a full report on the project, including a
    write-up (report.pdf) and presentation (presentation.pptx). Finally,
    some basic analysis on exported data in the form of an excel
    spreadsheet in included (GasData.xlsx).

--- program.c
+++ program.c
@@ -85,7 +85,7 @@ int main()
 	}
 
 	//Write contents of store array to text file
-	FILE* txt = fopen("info", "w");	
+	FILE* txt = fopen("tmp", "w");	
 	for(i = 0; i < 40; i++){
 		for(k = 0; k < 210; k++){
 			fprintf(txt,"%c",setup[i][k]);
@@ -96,57 +96,72 @@ int main()
 
 	//Open text file and use fscanf to read each data point in, ignoring
 	//white space
-	FILE* read = fopen("info", "r");
+	FILE* read = fopen("tmp", "r");
 	for(i = 0; i < 40; i++){
 		fscanf(read, " %d %f %f %f %f %f %f %f %f %f %f %f %f", &year, &val1, &val2, &val3, &val4, &val5, &val6, &val7, &val8, &val9, &val10, &val11, &val12);
 		for(k = 0; k < 12; k++){
 			
 			finalI[i] = year;
-			if(k == 0){ //Jan
+			if(k == 0)
+			{ //Jan
 				final[i][k] = val1;
 			}
-			if(k == 1){ //Feb
+			if(k == 1)
+			{ //Feb
 				final[i][k] = val2;
 			}
-			if(k == 2){ //Mar
+			if(k == 2)
+			{ //Mar
 				final[i][k] = val3;
 			}
-			if(k == 3){ //April
+			if(k == 3)
+			{ //April
 				final[i][k] = val4;
 			}
-			if(k == 4){ //May
+			if(k == 4)
+			{ //May
 				final[i][k] = val5;
 			}
-			if(k == 5){ //June
+			if(k == 5)
+			{ //June
 				final[i][k] = val6;
 			}
-			if(k == 6){ //July
+			if(k == 6)
+			{ //July
 				final[i][k] = val7;
 			}
-			if(k == 7){ //Aug
+			if(k == 7){
+			 //Aug
 				final[i][k] = val8;
 			}
-			if(k == 8){ //Sep
+			if(k == 8)
+			{ //Sep
 				final[i][k] = val9;
 			}
-			if(k == 9){ //Oct
+			if(k == 9)
+			{ //Oct
 				final[i][k] = val10;
 			}
-			if(k == 10){ //Nov
+			if(k == 10)
+			{ //Nov
 				final[i][k] = val11;
 			}
-			if(k == 11){ //Dec
+			if(k == 11)
+			{ //Dec
 				final[i][k] = val12;
 			}
 		}		
 	}
+	fclose(read);
 
 	//Print final results for testing
 	printf("Year\t Jan\t Feb\t Mar\t April\t May\t June\t July\t Aug\t Sep\t Oct\t Nov\t Dec\t\n");
 	printf("*****************************************************************************************************\n");
-	for(i = 0; i < 40; i++){
+	for(i = 0; i < 40; i++)
+	{
 		printf("%4d\t", finalI[i]);
-		for(k = 0; k < 12; k++){
+		for(k = 0; k < 12; k++)
+		{
 			printf("%.3f\t",final[i][k]);
 		}
 		printf("\n");
@@ -155,7 +170,90 @@ int main()
 	/*******************************************************************/
 	/*PART TWO- Use final arrays to create an XML form for Excel import*/
 	/*******************************************************************/
+	
+	//Create File
+	FILE *xml =fopen("GasData.xml", "w");
+	
+	//Print XML file
 
+	//Header Junk
+	fprintf(xml, "<?xml version=\"1.0\"?>\n");
+	fprintf(xml,"<?mso-application progid=\"Excel.Sheet\"?>\n");
+	fprintf(xml,"<Workbook\n");;
+		fprintf(xml,"   xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\"\n");
+		fprintf(xml,"   xmlns:o=\"urn:schemas-microsoft-com:office:office\"\n");
+		fprintf(xml,"   xmlns:x=\"urn:schemas-microsoft-com:office:excel\"\n");
+		fprintf(xml,"   xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\"\n");
+		fprintf(xml,"   xmlns:html=\"http://www.w3.org/TR/REC-html40\">\n");
+		fprintf(xml,"  <DocumentProperties xmlns=\"urn:schemas-microsoft-com:office:office\">\n");
+			fprintf(xml,"    <Author>wallacs5</Author>\n");
+			fprintf(xml,"    <LastAuthor>Self</LastAuthor>\n");
+			fprintf(xml,"    <Created>2016-05-12T13:38:04Z</Created>\n");
+			fprintf(xml,"    <Company>Miami University</Company>\n");
+			fprintf(xml,"    <Version>11.8036</Version>\n");
+		fprintf(xml,"  </DocumentProperties>\n");
+		fprintf(xml,"  <ExcelWorkbook xmlns=\"urn:schemas-microsoft-com:office:excel\">\n");
+			fprintf(xml,"    <WindowHeight>6795</WindowHeight>\n");
+			fprintf(xml,"    <WindowWidth>8460</WindowWidth>\n");
+			fprintf(xml,"    <WindowTopX>120</WindowTopX>\n");
+			fprintf(xml,"    <WindowTopY>15</WindowTopY>\n");
+			fprintf(xml,"    <ProtectStructure>False</ProtectStructure>\n");
+			fprintf(xml,"    <ProtectWindows>False</ProtectWindows>\n");
+		fprintf(xml,"  </ExcelWorkbook>\n");
+		fprintf(xml,"  <Styles>\n");
+			fprintf(xml,"    <Style ss:ID=\"Default\" ss:Name=\"Normal\">\n");
+				fprintf(xml,"      <Alignment ss:Vertical=\"Bottom\" />\n");
+				fprintf(xml,"      <Borders />\n");
+				fprintf(xml,"      <Font />\n");
+				fprintf(xml,"      <Interior />\n");
+				fprintf(xml,"      <NumberFormat />\n");
+				fprintf(xml,"      <Protection />\n");
+			fprintf(xml,"    </Style>\n");
+		fprintf(xml,"  </Styles>\n");
+
+		//Writing Worksheet info
+		fprintf(xml,"\t<Worksheet ss:Name=\"Sheet1\">\n");
+			fprintf(xml,"\t\t<Table>\n");
+
+				for(int i=0;i<40;i++)//Write rows
+				{
+					fprintf(xml,"\t\t\t<Row>\n");
+						fprintf(xml,"\t\t\t\t<Cell>\n");
+							fprintf(xml,"\t\t\t\t\t<Data ss:Type=\"Number\">%d</Data>\n",finalI[i]);
+							fprintf(xml,"\t\t\t\t</Cell>\n");
+					for(int j=0;j<12;j++)//Write columns
+					{
+						fprintf(xml,"\t\t\t\t<Cell>\n");
+							fprintf(xml,"\t\t\t\t\t<Data ss:Type=\"Number\">%f</Data>\n",final[i][j]);
+						fprintf(xml,"\t\t\t\t</Cell>\n");
+					}
+					fprintf(xml,"</Row>\n");
+				}
+			fprintf(xml,"\t\t</Table>\n");
+		//End writing worksheet
+		fprintf(xml,"\t\t<WorksheetOptions xmlns=\"urn:schemas-microsoft-com:office:excel\">\n");	
+			fprintf(xml,"\t\t\t<Print>\n");	
+				fprintf(xml,"\t\t\t\t<ValidPrinterInfo />\n");
+				fprintf(xml,"\t\t\t\t<HorizontalResolution>600</HorizontalResolution>\n");
+				fprintf(xml,"\t\t\t\t<VerticalResolution>600</VerticalResolution>\n");	
+			fprintf(xml,"\t\t\t</Print>\n");
+			fprintf(xml,"\t\t\t<Selected />\n");
+			fprintf(xml,"\t\t\t<Panes>\n");
+				fprintf(xml,"\t\t\t\t<Pane>\n");
+					fprintf(xml,"\t\t\t\t\t<Number>3</Number>\n");
+					fprintf(xml,"\t\t\t\t\t<ActiveRow>5</ActiveRow>\n");
+					fprintf(xml,"\t\t\t\t\t<ActiveCol>1</ActiveCol>\n");
+				fprintf(xml,"\t\t\t\t</Pane>\n");
+			fprintf(xml,"\t\t\t</Panes>\n");
+			fprintf(xml,"\t\t\t<Table>\n");
+			fprintf(xml,"\t\t\t</Table>\n");
+			fprintf(xml,"\t\t\t<ProtectObjects>False</ProtectObjects>\n");
+			fprintf(xml,"\t\t\t<ProtectScenarios>False</ProtectScenarios>\n");
+		fprintf(xml,"\t\t\t<ProtectScenarios>False</ProtectScenarios>\n");
+		fprintf(xml,"\t\t</WorksheetOptions>\n");
+	fprintf(xml,"\t</Worksheet>\n");
+	fprintf(xml,"</Workbook>");
+	fclose(xml);
 
 
 	return(0);
